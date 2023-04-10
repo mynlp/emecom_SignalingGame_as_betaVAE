2869187640d5561742ca1d12f64eca8c51be7541
wedddy0707 <ryoryo.ueda@gmail.com>
2023-04-10 17:56:56 +0900

    implement accuracy based beta scheduler

--- emecom_gen/model/game/beta_scheduler.py
+++ emecom_gen/model/game/beta_scheduler.py
@@ -1,3 +1,4 @@
+from torch import Tensor
 from torch.nn import Module
 import torch
 
@@ -6,7 +7,7 @@ class BetaSchedulerBase(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, step: int) -> float:
+    def forward(self, step: int, acc: Tensor) -> float:
         raise NotImplementedError()
 
 
@@ -18,7 +19,7 @@ class ConstantBetaScheduler(BetaSchedulerBase):
         super().__init__()
         self.value = value
 
-    def forward(self, step: int) -> float:
+    def forward(self, step: int, acc: Tensor) -> float:
         return self.value
 
 
@@ -32,5 +33,27 @@ class SigmoidBetaScheduler(BetaSchedulerBase):
         self.gain = gain
         self.offset = offset
 
-    def forward(self, step: int) -> float:
+    def forward(self, step: int, acc: Tensor) -> float:
         return torch.as_tensor(self.gain * (step - self.offset), dtype=torch.float).sigmoid().item()
+
+
+class AccuracyBasedBetaScheduler(BetaSchedulerBase):
+    def __init__(
+        self,
+        exponent: float = 10,
+        smoothing_factor: float = 0.1,
+    ) -> None:
+        super().__init__()
+        self.exponent = exponent
+        self.smoothing_factor = smoothing_factor
+        self.acc_ema = None
+
+    def forward(self, step: int, acc: Tensor) -> float:
+        if self.training:
+            if self.acc_ema is None:
+                self.acc_ema = acc.mean().item()
+            else:
+                self.acc_ema = self.smoothing_factor * acc.mean().item() + (1 - self.smoothing_factor) * self.acc_ema
+            return self.acc_ema**self.exponent
+        else:
+            return 0 if self.acc_ema is None else self.acc_ema**self.exponent
