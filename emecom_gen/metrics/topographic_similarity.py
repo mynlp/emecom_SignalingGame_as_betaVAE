aae30a8357193a82997eb697bcf75b7730b9a08f
wedddy0707 <ryoryo.ueda@gmail.com>
2023-04-10 17:56:20 +0900

    modify inputs to sender

--- emecom_gen/metrics/topographic_similarity.py
+++ emecom_gen/metrics/topographic_similarity.py
@@ -103,7 +103,7 @@ class TopographicSimilarity(Callback):
                 batch: Batch = batch.to(pl_module.device)
 
                 for sender_idx, sender in list(enumerate(pl_module.senders)):
-                    sender_output = sender.forward(batch.input)
+                    sender_output = sender.forward(batch)
 
                     messages[sender_idx].extend((sender_output.message * sender_output.message_mask).tolist())
                     message_lengths[sender_idx].extend(sender_output.message_length.tolist())
