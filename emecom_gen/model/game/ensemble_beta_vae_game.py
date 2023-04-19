497b79c205c929b1aaebda16f3d895cee384510f
wedddy0707 <ryoryo.ueda@gmail.com>
2023-04-20 00:06:44 +0900

    modify baseline computation

--- emecom_gen/model/game/ensemble_beta_vae_game.py
+++ emecom_gen/model/game/ensemble_beta_vae_game.py
@@ -135,8 +135,8 @@ class EnsembleBetaVAEGame(GameBase):
         surrogate_loss = (
             loss_r * update_r
             + loss_p * update_p
-            + loss_s.detach() * (output_s.message_log_probs * mask).sum(dim=-1) * update_s
-            + loss_s.pow(2) * update_s
+            + ((loss_s - baseline.detach()) / denominator) * (output_s.message_log_probs * mask).sum(dim=-1) * update_s
+            + (loss_s - baseline).pow(2) * update_s
         )
 
         return GameOutput(
