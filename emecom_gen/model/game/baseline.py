d3803140cb39ee161fb53c1afe220f23039c496a
wedddy0707 <ryoryo.ueda@gmail.com>
2023-05-17 14:26:24 +0900

    modify input-dependent baseline

--- emecom_gen/model/game/baseline.py
+++ emecom_gen/model/game/baseline.py
@@ -1,22 +1,34 @@
-from torch.nn import Module, Linear
+from torch.nn import Module, Linear, Embedding, Parameter
 from torch import Tensor
+import torch
 
 
 class InputDependentBaseline(Module):
     def __init__(
         self,
         in_features: int,
+        max_len: int,
         num_senders: int = 1,
         num_receivers: int = 1,
     ) -> None:
         super().__init__()
-        self.layers = {
-            (i, j): Linear(in_features=in_features, out_features=1)
-            for i in range(num_senders)
-            for j in range(num_receivers)
-        }
-        for k, v in self.layers.items():
-            self.add_module(str(k), v)
+        self.input_to_baseline = Linear(in_features, 1)
+        self.position_to_baseline = Parameter(torch.zeros(max_len))
+
+        if num_senders > 1:
+            self.sender_idx_to_baseline = Embedding(num_senders, 1)
+        else:
+            self.sender_idx_to_baseline = None
+
+        if num_receivers > 1:
+            self.receiver_idx_to_baseline = Embedding(num_receivers, 1)
+        else:
+            self.receiver_idx_to_baseline = None
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.normal_(self.position_to_baseline)
 
     def forward(
         self,
@@ -24,4 +36,29 @@ class InputDependentBaseline(Module):
         sender_idx: int = 0,
         receiver_idx: int = 0,
     ):
-        return self.layers[sender_idx, receiver_idx].forward(input.flatten(start_dim=1)).squeeze(-1)
+        batch_size = input.shape[0]
+        device = input.device
+
+        baseline = self.input_to_baseline.forward(input) + self.position_to_baseline.unsqueeze(0).reshape(
+            batch_size, -1
+        )
+        if self.sender_idx_to_baseline is not None:
+            baseline = baseline + self.sender_idx_to_baseline.forward(
+                torch.full(
+                    size=(batch_size, 1),
+                    fill_value=sender_idx,
+                    dtype=torch.long,
+                    device=device,
+                )
+            )
+        if self.receiver_idx_to_baseline is not None:
+            baseline = baseline + self.receiver_idx_to_baseline.forward(
+                torch.full(
+                    size=(batch_size, 1),
+                    fill_value=receiver_idx,
+                    dtype=torch.long,
+                    device=device,
+                )
+            )
+
+        return baseline
