fb341ac3538442b7c43c7519072ef2ea0e9fa3a5
wedddy0707 <ryoryo.ueda@gmail.com>
2023-05-26 17:53:22 +0900

    modify definition of dataclasses

--- emecom_gen/model/game/game_output.py
+++ emecom_gen/model/game/game_output.py
@@ -1,4 +1,5 @@
 from torch import Tensor
+from typing import Optional
 import dataclasses
 
 from ..sender import SenderOutput, SenderOutputGumbelSoftmax
@@ -11,30 +12,42 @@ class GameOutput:
     loss: Tensor
     communication_loss: Tensor
     acc: Tensor
-    sender_output: SenderOutput
-    receiver_output: ReceiverOutput
-    message_prior_output: MessagePriorOutput
+    sender_output: Optional[SenderOutput] = None
+    receiver_output: Optional[ReceiverOutput] = None
+    message_prior_output: Optional[MessagePriorOutput] = None
 
     def make_log_dict(
         self,
         prefix: str = "",
         suffix: str = "",
     ):
-        log_dict = {
+        log_dict: dict[str, Tensor] = {
             "surrogate_loss": self.loss,
             "communication_loss": self.communication_loss,
             "acc": self.acc,
-            "message_length": self.sender_output.message_length,
-            "sender_message_nll": (self.sender_output.message_log_probs * self.sender_output.message_mask)
-            .sum(dim=-1)
-            .neg(),
-            "prior_message_nll": (self.message_prior_output.message_log_probs * self.sender_output.message_mask)
-            .sum(dim=-1)
-            .neg(),
-            "message_entropy": self.sender_output.message_entropy,
-            "entropy": self.sender_output.entropies,
-            "normalized_entropy": self.sender_output.normalized_entropies,
         }
+
+        if self.sender_output is not None:
+            log_dict.update(
+                {
+                    "message_length": self.sender_output.message_length,
+                    "sender_message_nll": (self.sender_output.message_log_probs * self.sender_output.message_mask)
+                    .sum(dim=-1)
+                    .neg(),
+                    "normalized_entropy": self.sender_output.normalized_entropies,
+                }
+            )
+            if self.message_prior_output is not None:
+                log_dict.update(
+                    {
+                        "prior_message_nll": (
+                            self.message_prior_output.message_log_probs * self.sender_output.message_mask
+                        )
+                        .sum(dim=-1)
+                        .neg(),
+                    }
+                )
+
         return {prefix + k + suffix: v.detach().float().mean() for k, v in log_dict.items()}
 
 
@@ -43,8 +56,8 @@ class GameOutputGumbelSoftmax:
     loss: Tensor
     communication_loss: Tensor
     acc: Tensor
-    sender_output: SenderOutputGumbelSoftmax
-    receiver_output: ReceiverOutputGumbelSoftmax
+    sender_output: Optional[SenderOutputGumbelSoftmax] = None
+    receiver_output: Optional[ReceiverOutputGumbelSoftmax] = None
 
     def make_log_dict(
         self,
@@ -55,7 +68,13 @@ class GameOutputGumbelSoftmax:
             "surrogate_loss": self.loss,
             "communication_loss": self.communication_loss,
             "acc": self.acc,
-            "entropy": self.sender_output.entropies,
-            "normalized_entropy": self.sender_output.normalized_entropies,
         }
+
+        if self.sender_output is not None:
+            log_dict.update(
+                {
+                    "normalized_entropy": self.sender_output.normalized_entropies,
+                }
+            )
+
         return {prefix + k + suffix: v.detach().float().mean() for k, v in log_dict.items()}
