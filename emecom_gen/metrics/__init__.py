12bc29edd4a211e509ff4a559a2da37ccbbed700
wedddy0707 <ryoryo.ueda@gmail.com>
2023-06-21 16:31:36 +0900

    modify import

--- emecom_gen/metrics/__init__.py
+++ emecom_gen/metrics/__init__.py
@@ -1,4 +1,4 @@
 from .topographic_similarity import TopographicSimilarity
 from .dump_language import DumpLanguage
 from .harris_scheme_based_metrics import HarrisSchemeBasedMetrics, HarrisScheme
-from .speakers_synchronization import SpeakersSynchronization
+from .language_similarity import LanguageSimilarity
